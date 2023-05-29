a308bc38c241b96c7c04758c9bf80310570c7d07
Enzo Cazenave <encaze24@hotmail.com>
2023-05-28 22:51:16 -0300

    Add changes

--- dolarhistorico/src/screens/SettingsScreen.tsx
+++ dolarhistorico/src/screens/SettingsScreen.tsx
@@ -1,20 +1,54 @@
-import { StyleSheet, Text, View, useColorScheme } from 'react-native'
+import { Linking, StyleSheet, Text, TouchableOpacity, View, useColorScheme } from 'react-native'
 import { colors } from '../data/colors'
+import Icon from 'react-native-vector-icons/Ionicons'
 
 export const SettingsScreen: React.FC = () => {
     const theme = useColorScheme() ?? 'light'
 
+    const navigateToGithub: () => void = () => {
+        const url = 'https://github.com/enzocazenave'
+
+        void Linking.canOpenURL(url).then(supported => {
+            if (supported) void Linking.openURL(url)
+        })
+    }
+
     return (
         <View style={[styles.container, { backgroundColor: colors[theme].background1 }]}>
-            <Text>
-                Settings
-            </Text>
+            <View style={[styles.copyrights, { backgroundColor: colors[theme].background2 }]}>
+                <Text style={{ color: colors[theme].color1 }}>
+                    Aplicación desarrollada con fines de aprendizaje y basada en <Text style={{ color: theme === 'dark' ? '#9f9' : '#00f' }}>Ámbito Dólar</Text>, que es otra aplicación que se utiliza para visualizar las cotizaciones del dólar.
+                </Text>
+            </View>
+            <TouchableOpacity
+                style={[styles.copyrights, { backgroundColor: colors[theme].background2 }]}
+                onPress={navigateToGithub}
+            >
+                <Text style={{ color: colors[theme].color1 }}>
+                    Desarrollador: <Text style={{ color: theme === 'dark' ? '#9f9' : '#00f' }}>Enzo Cazenave</Text>
+                </Text>
+                <Icon
+                    name="chevron-forward-outline"
+                    size={20}
+                    color="#aaa"
+                />
+            </TouchableOpacity>
+
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        height: '100%'
+        padding: 20,
+        height: '100%',
+        gap: 20
+    },
+    copyrights: {
+        padding: 20,
+        borderRadius: 10,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
     }
 })
